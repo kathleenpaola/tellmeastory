5e63705e562a9c5b88570274c1534868ddff717e
Kathleen <kathleen.paola@gmail.com>
2014-05-16 00:30:45 +0000

    created posts model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tellmeastory::Application.config.secret_token = '7cc7127b4163c417aedd3765f7673467332026acf3218184899811301fe4fae4460fa8ab4a339d3c7ee1c4a4a05823e69224b138fc8c3889140de3dba599e3a5'
